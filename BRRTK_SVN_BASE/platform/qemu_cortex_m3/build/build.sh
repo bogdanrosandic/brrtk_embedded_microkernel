85879f2be058e71becbd5eb4fd2e19e7ad4ce3f3
brosandic <54672724+bogdanrosandic@users.noreply.github.com>
2012-09-25 16:11:12 +0000

    Last modifications at work. Should be checked!

--- BRRTK_SVN_BASE/platform/qemu_cortex_m3/build/build.sh
+++ BRRTK_SVN_BASE/platform/qemu_cortex_m3/build/build.sh
@@ -2,5 +2,5 @@
 export PATH=/home/renesas/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_GNU_Linux/arm-none-linux-gnueabi/bin/:/home/renesas/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_GNU_Linux/bin:$PATH
 
 make brrtk.o
-make platform.o
-make my_program
+#make platform.o
+#make my_program
